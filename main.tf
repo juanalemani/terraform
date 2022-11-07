028e747dae1dd0b07386eea8624771bb5e5d1f83
juanalemani <juanignacioalemani@gmail.com>
2022-11-07 13:37:35 -0300

    Se añade rout table

--- main.tf
+++ main.tf
@@ -26,3 +26,17 @@ resource "aws_internet_gateway" "mtc_igw" {
         name = "dev-igw"
     }
 }
+
+resource "aws_route_table" "mtc_public_route_table" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  
+    tags = {
+        name = "dev-public_rt"
+    }
+}
+
+resource "aws_route" "mtc_public_route" {
+  route_table_id = aws_route_table.mtc_public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_igw.id
+}
