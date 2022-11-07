84463cff03406766684cd1bd631cd768d535466f
juanalemani <juanignacioalemani@gmail.com>
2022-11-07 11:26:50 -0300

    se añade una vpc

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "mtc_vpc" {
         name = "dev"
     }
 }
+
+resource "aws_subnet" "mtc_public_subnet" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  cidr_block = 10.123.1.0/24  
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  
+    tags = {
+        name = "dev-public"
+    }
+}
